90732de98a8cd8c7983bea065d517068c1622c74
Ryan Harrigan <ryanharrigan9876@gmail.com>
2020-06-06 22:58:09 +0100

    Pluralsight course

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -14,14 +14,19 @@ func RootHandler(res http.ResponseWriter, req *http.Request) {
 func ListWinners(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "application/json")
 
-	winners, err := data.ListAllJSON()
+	year := req.URL.Query().Get("year")
 
-	if err != nil {
-		res.WriteHeader(http.StatusInternalServerError)
-		return
+	if year == "" {
+		winners, err := data.ListAllJSON()
+
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		res.Write(winners)
 	}
 
-	res.Write(winners)
 }
 
 // AddNewWinner adds new winner to the list
